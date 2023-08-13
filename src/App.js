c1e190955f79f7d9cb6f8d58015b51c174cf367f
Mert Eren Keküç <merterenkekuc@gmail.com>
2023-08-13 23:01:36 +0300

    ilk commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const App = () => {
+    return(
+        <View>
+            <View>
+                <Text>Yapılacaklar</Text>
+            </View>
+            <View>
+                <Text>ToDo lar</Text>
+            </View>
+        </View>
+    )
+}
+
+export default App;
